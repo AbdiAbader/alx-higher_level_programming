cd3392095833cfd3a63aa08d10b1577a6d306307
AbdiAbader <abdiprocode@gmail.com>
2022-08-22 19:11:06 +0300

    Task 9 -10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+*check_cycle - checks for cycle in linked list
+*@list: list of  or head of the list
+*Return: 0 if no cycle otherwise 1
+*/
+int check_cycle(listint_t *list)
+{
+listint_t *prt1 = list, *prt2 = list;
+while (prt1->next != NULL && prt2->next->next != NULL)
+{
+prt1 = prt1->next;
+prt2 = prt2->next->next;
+if (prt1 == prt2)
+return (1);
+}
+return (0);
+}
