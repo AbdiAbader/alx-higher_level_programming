13c438c049a9ea359c8f160d106cc2b8b0edd040
AbdiAbader <abdiprocode@gmail.com>
2022-08-27 17:29:10 +0300

    fix error on loop

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,14 +1,14 @@
 #include "lists.h"
 #include <stdio.h>
 /**
- *is_palindorome - palindrome checker
- *@head: head of list
- *Return: 1 if palindrome  otherwise 0
- */
+*is_palindorome - palindrome checker
+*@head: head of list
+*Return: 1 if palindrome  otherwise 0
+*/
 int is_palindrome(listint_t **head)
 {
-  int len = 0, i = 0;
-  listint_t *hold1 , *tail ,*temp ,*hold2;
+int len = 0, i = 0;
+ listint_t *hold1 , *tail ,*temp ,*hold2 , *a;
 if (*head == NULL)
 return 1;
 tail = malloc(sizeof(listint_t));
@@ -17,33 +17,33 @@ return 0;
 hold1 = *head;
 temp = *head;
 while (temp)
-   {
-   if ( len == 0)
-     {
-	 tail->n = temp->n;
-	 tail->next = NULL;
-	 hold2 = tail;
-       }
+{
+if ( len == 0)
+{
+tail->n = temp->n;
+tail->next = NULL;
 
-   else
-     {
-       temp = temp->next;
-       tail->next = temp;
-       tail = temp;
-      
-     }
-   len++;
-   }
-     while (hold1 && hold2)
-       {
-	 if ( i == len / 2)
-	   break;
-	   
-	 if (hold1->n != hold2->n)
-	   return 0;
-	 hold1 = hold1->next;
-	 hold2 = hold2->next;
-	 i++;
-       }
- return 1;
+}
+
+else
+{
+a = temp;
+temp = temp->next;
+a->next = tail;
+tail = a;
+}
+len++;
+}
+hold2 =  tail;
+while (hold1 && hold2)
+{
+if ( i == len / 2)
+break;
+if (hold1->n != hold2->n)
+return 0;
+hold1 = hold1->next;
+hold2 = hold2->next;
+i++;
+}
+return 1;
 }
