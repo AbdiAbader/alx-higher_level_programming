0332664aaae6e81bed62a6fba7b43d96cdbbbe76
AbdiAbader <abdiprocode@gmail.com>
2022-08-28 09:57:55 +0300

    fix error head address

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,49 +1,52 @@
 #include "lists.h"
-#include <stdio.h>
 /**
 *is_palindorome - palindrome checker
 *@head: head of list
-*Return: 1 if palindrome  otherwise 0
+*Return: 1 if palindrome  otherwise 
 */
 int is_palindrome(listint_t **head)
 {
 int len = 0, i = 0;
- listint_t *hold1 , *tail ,*temp ,*hold2 , *a;
-if (*head == NULL)
-return 1;
-tail = malloc(sizeof(listint_t));
-if (tail == NULL)
-return 0;
+ listint_t *hold1 , *tail ,*temp, *hold2;
+if (*head == NULL || (*head)->next == NULL)
+return (1);
 hold1 = *head;
 temp = *head;
+tail = NULL;
 while (temp)
 {
-if ( len == 0)
-{
-tail->n = temp->n;
-tail->next = NULL;
-
-}
-
-else
-{
-a = temp;
+first(&tail, temp->n);
 temp = temp->next;
-a->next = tail;
-tail = a;
-}
 len++;
 }
-hold2 =  tail;
-while (hold1 && hold2)
+hold2 = tail;
+while (hold1)
 {
 if ( i == len / 2)
 break;
 if (hold1->n != hold2->n)
-return 0;
+return (0);
 hold1 = hold1->next;
 hold2 = hold2->next;
 i++;
 }
-return 1;
+free_listint(tail);
+return (1);
+}
+/**
+ *first - add node at the head
+ *@head: head of linked list
+ *@n: number
+ *Return: address of new list
+ */
+listint_t *first(listint_t **head, const int n)
+{
+listint_t *temp;
+temp = malloc(sizeof(listint_t));
+if (temp == NULL)
+return (NULL);
+temp->n = n;
+temp->next = *head;
+*head = temp;
+return (temp);
 }
