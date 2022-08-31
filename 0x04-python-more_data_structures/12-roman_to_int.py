9a93c6292aa5c42a48397121549da2c361a50b30
-v <abdiprocode@gmail.com>
2022-08-31 13:40:57 +0300

    fix on sum

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    sum = 0
+    sum1 = 0
+    j = 0
     rome = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    if isinstance(roman_string, str):
-        for i in roman_string:
+    if type(roman_string) is str and not None:
+        r = list(reversed(list(roman_string)))
+        for i in r:
             for keys, values in rome.items():
                 if i == keys:
-                    sum += values
-                else:
-                    return 0
-        return sum
+                    if values >= j:
+                        sum1 += values
+                        j = values
+                    else:
+                        sum1 -= values
+        return sum1
     else:
-        return sum
+        return sum1
