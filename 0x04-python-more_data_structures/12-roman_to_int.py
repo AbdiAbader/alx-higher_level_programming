c08cee322e91a553bb546ca8e88a30c74445c75f
-v <abdiprocode@gmail.com>
2022-08-31 13:15:22 +0300

    fix on return value

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     sum = 0
     rome = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    if isinstance(roman_string, str) and roman_string is not None:
+    if isinstance(roman_string, str):
         for i in roman_string:
             for keys, values in rome.items():
                 if i == keys:
