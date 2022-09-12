3b8233b1278d43cca70efe9ad474563660df2c00
-v <abdiprocode@gmail.com>
2022-09-12 12:13:19 +0300

    update readme

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,13 @@
-#!/usr/bin/pyyhon3
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    y = 0
-    for i in range(0, x):
+    c = y = 0
+    for i in my_list:
+        c += 1
+    for i in range(x):
         try:
             print("{}".format(my_list[i]), end='')
             y += 1
-        except:
+        except IndexError:
             break
-            return y
     print("")
     return y
