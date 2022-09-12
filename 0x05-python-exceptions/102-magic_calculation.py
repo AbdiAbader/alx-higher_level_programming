c225bbad6709e941c2de2cbceb52808292ad5542
-v <abdiprocode@gmail.com>
2022-09-12 15:49:43 +0300

    magic file

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
     for i in range(1, 3):
-        i += 1
-    if i > a:
-       raise("To far")
-    else:
-        result = a ** b
-    result /= i
-    result += a+b
+        try:
+            if i > a:
+                raise Exception("To far")
+            else:
+                result = a ** b
+                result /= i
+        except:
+            result = a + b
+            break
     return result
