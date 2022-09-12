80fcc5c73a0254495e30c8af748a456f9d8d5346
-v <abdiprocode@gmail.com>
2022-09-12 12:45:40 +0300

    task 4

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    """Returns a new list (length = list_length) with all divisions"""
+    a = []
+    x = 0
+    for i in range(list_length):
+        try:
+            x = my_list_1[i] / my_list_2[i]
+        except IndexError:
+            print("wrong type")
+            x = 0
+        except TypeError:
+            print("out of range")
+            x = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            x = 0
+        finally:
+            a.append(x)
+    return a
