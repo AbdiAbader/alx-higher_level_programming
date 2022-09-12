909fb270b9032ed44a58940906be1811d8125639
-v <abdiprocode@gmail.com>
2022-09-12 11:55:02 +0300

    Read and task 1

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+def safe_print_list(my_list=[], x=0):
+    y = 0
+    for i in range(0, x):
+        try:
+            print("{}".format(my_list[i]), end='')
+            y += 1
+        except IndexError:
+            break
+    print("")
+    return y
