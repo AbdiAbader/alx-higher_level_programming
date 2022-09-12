0ab8dbe3380594237d027c32d86627153ce672cc
-v <abdiprocode@gmail.com>
2022-09-12 13:20:32 +0300

    Task 100

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,14 @@
+import sys
+
+
+def safe_print_integer_err(value):
+    """Returns True if value has been correctly printed
+     (it means the value is an integer)
+     Otherwise, returns False and prints in stderr the error precede by
+     Exception:"""
+    try:
+        print("{:d}".format(value))
+    except (ValueError, TypeError) as a:
+        sys.stderr.write("Exception: {}\n".format(a))
+        return False
+    return True
