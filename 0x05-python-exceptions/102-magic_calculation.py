1eb33746e76c765ec6315d0c2e77a70a2d84fd62
-v <abdiprocode@gmail.com>
2022-09-12 16:20:06 +0300

    fix error on result initial

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -7,7 +7,7 @@ def magic_calculation(a, b):
                 raise Exception("Too far")
             else:
                 result += a ** b / i
-        except (ZeroDivisionError):
+        except Exception:
             result = a + b
             break
     return result
