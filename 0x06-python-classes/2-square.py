206bed7524509a10686dae26944418d1b74f46ac
-v <abdiprocode@gmail.com>
2022-09-13 15:53:07 +0300

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,6 +8,6 @@ class Square:
         """ initialization """
         if size < 0:
             raise ValueError("size must be >= 0")
-        elif type(size) != int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         self.__size = size
