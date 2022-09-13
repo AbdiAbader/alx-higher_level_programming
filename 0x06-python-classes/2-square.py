3992380670a50ade50d850ab5634c1c1f0a54c2f
-v <abdiprocode@gmail.com>
2022-09-13 15:56:20 +0300

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,8 +6,8 @@ class Square:
     """ Class with Init method and Exeception try / execpt """
     def __init__(self, size=0):
         """ initialization """
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        if not isinstance(size, int):
+        if type(size) != int:
             raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
