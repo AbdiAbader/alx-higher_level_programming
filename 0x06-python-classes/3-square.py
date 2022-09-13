7ec061b3f7d52b3ea359fac03201649f1582d037
-v <abdiprocode@gmail.com>
2022-09-13 16:06:07 +0300

    task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Create Class for square and add Exeception to size """
+
+
+class Square:
+    """ Class with Init method and Exeception try / execpt """
+    def __init__(self, size=0):
+        """ initialization """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Area calculation"""
+
+        return self.__size ** 2
