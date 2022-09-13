2db1688d6f62c663a907c7cc53a23351d1be2e17
-v <abdiprocode@gmail.com>
2022-09-13 15:37:04 +0300

    task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Create Class for square and add Exeception to size """
+
+
+class Square:
+    """ Class with Init method and Exeception try / execpt """
+    def __init__(self, size=0):
+        """ initialization """
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        elif type(size) != int:
+            raise TypeError("size must be an integer")
+        self.__size = size
