6eabd9bde105acddf7cce67c671f797f94bc0092
-v <abdiprocode@gmail.com>
2022-09-19 13:22:07 +0300

    fix on width =0

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -42,4 +42,6 @@ class Rectangle:
 
     def parameter(self):
         """ parameter of rectangle """
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return self.__width * 2 + self.__height * 2
