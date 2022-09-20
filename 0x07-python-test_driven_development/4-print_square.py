119a8e016510b7b05d42df47bcf04a3a56975598
-v <abdiprocode@gmail.com>
2022-09-20 22:57:33 +0300

    task 4 first commit

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Prints Squares module
+prints Square
+"""
+
+
+def print_square(size):
+    """ prints square """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if size >= 0:
+        print(("#" * size + "\n") * size, end='')
