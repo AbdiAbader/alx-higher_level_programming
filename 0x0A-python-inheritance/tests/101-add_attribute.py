91929622b183f3f8f4510626d17d5d0a04344800
-v <abdiprocode@gmail.com>
2022-09-26 22:50:44 +0300

    task 101

---
+++ 0x0A-python-inheritance/tests/101-add_attribute.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" changes attributes  if possible """
+
+
+def add_attribute(objects, attribute, value):
+    """ add new attributes usaing setatrr() function
+        abnd check possible using hasatr functiom
+    """
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    setattr(objects, attribute, value)
