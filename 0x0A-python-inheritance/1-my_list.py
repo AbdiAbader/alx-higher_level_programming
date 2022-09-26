63c78b1c2f7ea711e2379bba76996d281273dc6c
-v <abdiprocode@gmail.com>
2022-09-26 12:16:23 +0300

    first commit for task 1:

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" inheritance class form list """
+
+
+class MyList(list):
+    """ inherits list """
+    def append(self, element):
+        return list.append(self, element)
+
+    def print_sorted(self):
+        """ print in sorted order """
+        s = self[:]
+        s.sort()
+        print(s)
