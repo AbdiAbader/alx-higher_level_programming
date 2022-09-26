a4af5b12e7e9571f6281e8ec8decea3435463c13
-v <abdiprocode@gmail.com>
2022-09-26 13:43:20 +0300

    first commit for task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" check inheirt from specified class """
+
+
+def is_kind_of_class(obj, a_class):
+    """ return True if inherit from specified class otherwise False"""
+    return isinstance(obj, a_class)
