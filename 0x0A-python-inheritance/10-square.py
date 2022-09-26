7f9bd74f3c91dc1ac3e65595750a4449484b197c
-v <abdiprocode@gmail.com>
2022-09-26 22:06:26 +0300

    task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Square functions """
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ class square """
+    def __init__(self, size):
+        """ """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """ area of square """
+        return self.__size ** 2
