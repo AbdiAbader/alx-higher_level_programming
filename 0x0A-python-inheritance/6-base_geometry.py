b4ee93bd234cb608e5d526cde76695ae6c638b58
-v <abdiprocode@gmail.com>
2022-09-26 16:35:01 +0300

    task 7

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Base Geometry """
+
+
+class BaseGeometry:
+    """ base geomotery Area checker """
+
+    def area(self):
+        """ area """
+        raise Exception("area() is not implemented")
