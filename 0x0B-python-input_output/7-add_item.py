2bdb90d9329cd44b2dc1116510f83bef7491213d
-v <abdiprocode@gmail.com>
2022-09-28 12:37:00 +0300

    add item add try/except

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,10 +9,9 @@ import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 filename = "add_item.json"
-a = []
-if len(sys.argv) != 1:
-    for i in range(1, len(sys.argv)):
-        a.append(sys.argv[i])
-save_to_json_file(a, filename)
-p = load_from_json_file(filename)
-print(p)
+try:
+    a = load_from_json_file(filename)
+except FileNotFoundError:
+    a = []
+    a.extend(sys.argv[1:])
+    save_to_json_file(items, filename)
