97aae32a133e5246ed34857be30f5bb10d93ce71
-v <abdiprocode@gmail.com>
2022-09-28 12:20:19 +0300

    fix error on return value:

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -7,5 +7,5 @@ import json
 
 def load_from_json_file(filename):
     """ creates object from JSON file """
-    with open(filename, 'r', encoding='utf-8') as f:
-        json.loads(f.read())
+    with open(filename) as f:
+        return json.load(f)
