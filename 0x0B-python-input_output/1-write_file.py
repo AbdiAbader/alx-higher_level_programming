5104e6df492e2dc3dcac13944ab598d8b4bbbe7d
-v <abdiprocode@gmail.com>
2022-09-28 12:04:30 +0300

    fix pep8

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,5 +4,5 @@
 
 def write_file(filename="", text=""):
     """ write to file """
-    with open(filename,'w', encoding='utf-8') as f:
+    with open(filename, 'w', encoding='utf-8') as f:
         return f.write(text)
