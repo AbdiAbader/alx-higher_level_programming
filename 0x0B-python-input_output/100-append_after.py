b100d148b656f6a34f5ba44ad6195e5682dba2bd
-v <abdiprocode@gmail.com>
2022-09-28 15:45:13 +0300

    fix code on write

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -4,11 +4,11 @@
 
 def append_after(filename="", search_string="", new_string=""):
     """ append after specific string """
-    with open(filename) as f:
+    with open(filename, 'r+') as f:
         t = ""
         for i in f:
             t += i
             if i in search_string:
                 t += new_string
-    with open(filename, 'w') as f:
+        f.seek(0)
         f.write(t)
