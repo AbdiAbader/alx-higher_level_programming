421e262e0f312ff3d4f6b5625d809e3add10f2da
-v <abdiprocode@gmail.com>
2022-09-28 12:17:42 +0300

    fix error on read

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -7,5 +7,5 @@ import json
 
 def load_from_json_file(filename):
     """ creates object from JSON file """
-    with open(filename, encoding='utf-8') as f:
+    with open(filename, 'r', encoding='utf-8') as f:
         json.loads(f.read())
