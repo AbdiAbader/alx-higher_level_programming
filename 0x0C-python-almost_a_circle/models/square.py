05e7ae5669e350398d41df8245a1f37b899ef913
-v <abdiprocode@gmail.com>
2022-10-03 19:05:50 +0300

    square file first commit

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+""" Square Module """
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ class Square that inherit Rectangle class """
+    def __init__(self, size, x=0, y=0, id=None):
+        """ constractor method of square """
+        super().__init__(size, size, x, y)
+
+    def __str__(self):
+        """ string represatation of class square """
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    @property
+    def size(self):
+        """ getter method """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """ upadate attributes value """
+        if args:
+            for i, j in enumerate(args):
+                if i == 0:
+                    self.id = j
+                elif i == 1:
+                    self.size = j
+                elif i == 2:
+                    self.x = j
+                elif i == 3:
+                    self.y = j
+        if kwargs:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "size" in kwargs:
+                self.size = kwargs["size"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
+
+    def to_dictionary(self):
+        """ prints in dic mode """
+        return {'id': self.id, 'x': self.x, 'size': self.width, 'y': self.y}
