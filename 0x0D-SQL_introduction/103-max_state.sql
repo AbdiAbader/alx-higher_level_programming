ff3d431dd8de7d88df9f9e43a80f185514fda525
-v <abdiprocode@gmail.com>
2022-11-01 23:28:22 +0300

    first commit for task 103

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,2 +1,2 @@
 -- max of al city
-SELECT city, (SELECT MAX(value) FROM temperatures) FROM temperatures GROUP BY city ORDER BY state DESC;
+SELECT state, (SELECT MAX(value) FROM temperatures) as max_temp FROM temperatures GROUP BY city ORDER BY state DESC;
