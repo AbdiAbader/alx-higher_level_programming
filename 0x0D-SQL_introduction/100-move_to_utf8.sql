2e7e818d4e79c91cdb4c476266ddc17a3af59a37
-v <abdiprocode@gmail.com>
2022-11-01 22:42:19 +0300

    add modify

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,2 @@
 -- changes to utf-8
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-ALTER TABLE first_table CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-ALTER TABLE first_table MODIFY name  CHARACTER SET utf8 NOT NULL COLLATE utf8_unicode_ci;
+USE temp ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
