3b382e234b52f441dc564ba901b18f57b7b6eb4d
-v <abdiprocode@gmail.com>
2022-11-01 22:29:25 +0300

    first commit for task 100

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,4 @@
+-- changes to utf-8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE first_table CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER FROM first_table name CHARACTER SET utf8 COLLATE utf8_unicode_ci;
