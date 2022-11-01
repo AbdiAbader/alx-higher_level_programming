ee89de34c3a2ec43259facdcd1a5a6dfb2c214ee
-v <abdiprocode@gmail.com>
2022-11-01 19:44:59 +0300

    fix code

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,2 +1,3 @@
 -- full creation
-SELECT INTO second_table FROM (VALUES(1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8)) as (id, name, score);
+CREATE TABLE second_table(id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table VALUES(1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
