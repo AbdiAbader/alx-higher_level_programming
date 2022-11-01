b390a5cb9428cd1fb90449ed0bdcf91811dbc8c7
-v <abdiprocode@gmail.com>
2022-11-01 22:02:37 +0300

    add oredr by desc

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- lists the number of records with the same score
 SELECT * FROM second_table;
-SELECT score, COUNT(*) AS  number FROM second_table GROUP BY score ORDER BY score DESC;
+SELECT score, COUNT(*) AS  number FROM second_table GROUP BY score ORDER BY number DESC;
