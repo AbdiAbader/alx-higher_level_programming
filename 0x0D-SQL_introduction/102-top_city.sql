75d8fd80f031bc631ea7a39a002df728737fc0be
-v <abdiprocode@gmail.com>
2022-11-01 23:22:15 +0300

    fix code ad AVg

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,2 @@
 -- tops 3 temprature
-SELECT city, avg_temp FROM tempratures where month = 7 AND month = 8 ORDER BY avg_temp DESC LIMIT 3
+SELECT city, AVG(value) as avg_temp FROM tempratures where month = 7 AND month = 8 ORDER BY avg_temp DESC LIMIT 3
