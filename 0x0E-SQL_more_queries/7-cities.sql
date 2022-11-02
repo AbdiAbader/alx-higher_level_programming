9a18f2870ed4f441d7d6a2a630eb01b1aa279e0d
-v <abdiprocode@gmail.com>
2022-11-02 21:12:57 +0300

    first commit for task 7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,3 @@
+--  creates the database hbtn_0d_usa and the table cities
+E DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(id INT NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, CONSTRAINT Customers_pk PRIMARY KEY (id), state_id INT NOT NULL FOREIGN KEY REFERENCES (state_id));
