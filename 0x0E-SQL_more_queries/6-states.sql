f5655896d9fd362cebb87a1e5bb69bf34e54acb9
-v <abdiprocode@gmail.com>
2022-11-02 20:44:19 +0300

    first commit for task 6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,4 @@
 -- creates database and tables
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+use hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states(id INT IDENTITY(1, 1) NOT NULL PRIMARY KEY, name VARCHAR(256) NOT NULL);
