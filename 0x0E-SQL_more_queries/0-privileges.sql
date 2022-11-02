d6314c158f90be3522a2d24c37c236592dd2e593
-v <abdiprocode@gmail.com>
2022-11-02 18:50:08 +0300

    first commit for task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
 -- list all users with privilege
-SELECT user_0d_1, user_0d_2 FROM information_schema.user_privileges;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
