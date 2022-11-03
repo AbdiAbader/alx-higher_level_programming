e5cddd3ed68a5099f292b266baf1eeff8d2075c4
-v <abdiprocode@gmail.com>
2022-11-03 05:03:16 +0300

    fix code on second select

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,4 @@
 -- script that lists all the cities of California
-SELECT id, name FROM cities WHERE states_id IN (SELECT id FROM states WHERE name = "California") ORDER BY id;
+SELECT id, name
+FROM cities WHERE states_id IN
+(SELECT id FROM states WHERE name = "California") ORDER BY id;
