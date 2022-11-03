68ee64c3112fc6711aa91029c5eb9465e872de7f
-v <abdiprocode@gmail.com>
2022-11-03 05:49:26 +0300

    task 11 -16

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++
@@ -1,6 +0,0 @@
--- Genre ID by show
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
