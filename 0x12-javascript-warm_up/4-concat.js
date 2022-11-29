56c056ebd95ee49e74c07e24e36a2d34516f2f6d
-v <abdiprocode@gmail.com>
2022-11-29 13:10:40 +0300

    fix style semi

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
 
-if (process.argv.length == 2)
-{
-console.log(process.argv[2] + " is " + process.argv[3]);
+if (process.argv.length === 2) {
+  console.log(process.argv[2] + ' is ' + process.argv[3]);
 }
-if (process.argv.length == 3)
-{
-console.log(process.argv[2] + " is " + process.argv[3]);
+if (process.argv.length === 3) {
+  console.log(process.argv[2] + ' is ' + process.argv[3]);
 }
-if (process.argv.length > 3)
-{
-console.log(process.argv[2] + " is " + process.argv[3]);
+if (process.argv.length > 3) {
+  console.log(process.argv[2] + ' is ' + process.argv[3]);
 }
