241774b230e254992049b0d5f2a779d4040d39a4
-v <abdiprocode@gmail.com>
2022-11-29 13:08:55 +0300

    fix semistand

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const c = "JavaScript is amazing",
-a = "C is fun", b = "Python is cool";
+const c = 'JavaScript is amazing';
+const a = 'C is fun'; const b = 'Python is cool';
 console.log(a);
 console.log(b);
 console.log(c);
