da351dadcac745287d161865cef371c29fa58f19
-v <abdiprocode@gmail.com>
2022-11-29 14:20:18 +0300

    final commit

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+if (isNaN(parseInt(process.argv[2])) || isNaN(parseInt(process.argv[3]))) {
+  console.log('NaN');
+} else {
+  console.log(parseInt(process.argv[2]) + parseInt(process.argv[3]));
+}
