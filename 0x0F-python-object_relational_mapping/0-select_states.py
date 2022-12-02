df235a168d8902aa4fbbae828b5c724cb1b4283f
-v <abdiprocode@gmail.com>
2022-12-02 18:54:10 +0300

    task-0 updated

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,9 +8,8 @@ if __name__ == '__main__':
             con = MySQLdb.connect("localhost",sys.argv[1], sys.argv[2], sys.argv[3])
             cur = con.cursor()
             command = cur.execute("select * from states")
-            cur.commit()
             for i in command:
                 print(i)
-            cur.close
+
 
 
