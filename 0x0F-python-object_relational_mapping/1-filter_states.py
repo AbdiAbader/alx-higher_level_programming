f21c9cd94ef685b1ac55862377d21334737ccde6
-v <abdiprocode@gmail.com>
2022-12-02 20:08:53 +0300

    fix sql statement:

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,5 +9,5 @@ if __name__ == '__main__':
     con = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = con.cursor()
     c = cur.execute("select * from states where name like 'N%'")
-    for i in c.fetchall():
+    for i in c:
         print(i)
