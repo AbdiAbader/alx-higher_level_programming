32eb28be30042663d6178edc325130de2bd61538
-v <abdiprocode@gmail.com>
2022-12-02 18:49:20 +0300

    task 0 and read me

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" lists all states from the database hbtn_0e_0_usa: """
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+            con = MySQLdb.connect("localhost",sys.argv[1], sys.argv[2], sys.argv[3])
+            cur = con.cursor()
+            command = cur.execute("select * from states")
+            cur.commit()
+            for i in command:
+                print(i)
+            cur.close
+
+
