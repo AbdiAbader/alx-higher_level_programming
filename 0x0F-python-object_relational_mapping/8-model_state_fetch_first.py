e077032b26eaf44da1f43b87e33614e6506802b6
-v <abdiprocode@gmail.com>
2022-12-03 22:15:27 +0300

    task 8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""  script that lists all State objects from the
+database hbtn_0e_6_usa """
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy.sql import select
+from sqlalchemy import create_engine, engine
+from sqlalchemy import text
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1],  sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    result = engine.execute(text("select * from states"))
+    for row in result:
+        if row:
+            print(f'{row.id}: {row.name}')
+            break
