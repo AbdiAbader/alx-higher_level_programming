dddce7ef310ed8bfdf4d81ca7c40ebe8ccb184a6
-v <abdiprocode@gmail.com>
2022-12-03 19:14:11 +0300

    creates table state

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""  python file that contains the class definition of a State and
+an instance Base = declarative_base()"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Table, Column, Integer, Numeric, String
+from sqlalchemy import create_engine
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
