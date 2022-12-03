54e97ad55991140394fa1d17c59e109e237df6e4
-v <abdiprocode@gmail.com>
2022-12-03 23:57:56 +0300

    task 12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" script that lists all State objects that contain the
+letter a from the database hbtn_0e_6_usa """
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy.engine import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1],  sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    r = session.query(State).filter(State.id == 2).first()
+    r.name = "New Mexico"
+    session.commit()
