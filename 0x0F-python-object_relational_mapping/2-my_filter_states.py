25f4834b4982e005e8419f5db2c0595bc3d5d9f4
-v <abdiprocode@gmail.com>
2022-12-03 06:34:02 +0300

    add format

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,6 +9,5 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     q = "SELECT * FROM states WHERE name = '{}'".format(sys.argv[4])
-    #c.execute(q, (sys.argv[4],))
     c.execute(q)
     [print(i) for i in c]
