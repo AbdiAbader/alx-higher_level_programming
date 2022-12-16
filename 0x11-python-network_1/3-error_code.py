505bad00f4f6f2dadbce13abd908a8509704f8b8
-v <abdiprocode@gmail.com>
2022-12-16 18:24:02 +0300

    update

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -7,8 +7,8 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen(sys.argv[1]) as response:
-        if response.code == 200:
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as response:
             print(response.read().decode("ascii"))
-        else:
-            print("Error code: {}".format(response.code))
+    except urllib.error.HTTPError as res:
+        print("Error code: {}".format(res.code))
