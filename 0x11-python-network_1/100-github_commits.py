c6e00aa815265511c88b2291f8f6a726da2b13df
-v <abdiprocode@gmail.com>
2022-12-16 19:43:31 +0300

    task 100:

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     commit = requests.get(
         f'https://api.github.com/repos/{argv[1]}/{argv[2]}/commits').json()
     for i in range(10):
-            print("{}: {}".format(
-                commit[i].get("sha"),
-                commit[i].get("commit").get("author").get("name")))
+        print("{}: {}".format(
+            commit[i].get("sha"),
+            commit[i].get("commit").get("author").get("name")))
