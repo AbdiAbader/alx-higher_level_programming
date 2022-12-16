581a3c63034c780758e815ed0bf45906633f1fd2
-v <abdiprocode@gmail.com>
2022-12-16 19:20:03 +0300

    task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""ython script that takes in a URL and an email, sends a POST
+request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)"""
+
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    quest = ""
+    url = 'http://0.0.0.0:5000/search_user'
+    if sys.argv[1]:
+        quest = sys.argv[1]
+    values = {"q": quest}
+    response = requests.post(url, data=values)
+    if r.json():
+        print(f'[{r.json().get("id")}] {r.json().get("name")}')
