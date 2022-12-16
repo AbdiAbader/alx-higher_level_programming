db7de088f864358ebe32166b4f1044d1b8a2fe41
-v <abdiprocode@gmail.com>
2022-12-16 18:27:47 +0300

    update

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -9,6 +9,6 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen(sys.argv[1]) as response:
         if response.code == 200:
-            print(response.read().decode("utf-8"))
+            print(response.read().decode('utf-8'))
         else:
             print("Error code: {}".format(response.code))
