2200e306dec834c1a43d5ce7d8edfcd6e063562b
-v <abdiprocode@gmail.com>
2022-12-16 18:33:50 +0300

    update

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -10,5 +10,5 @@ if __name__ == "__main__":
     try:
         with urllib.request.urlopen(argv[1]) as response:
             print(response.read().decode('utf-8'))
-    except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.code))
+    except urllib.error.HTTPError as err:
+        print("Error code: {}".format(err.code))
