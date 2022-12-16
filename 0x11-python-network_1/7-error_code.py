92bcfd4e900c13458ea01ecbfacfaa0087238231
-v <abdiprocode@gmail.com>
2022-12-16 19:10:11 +0300

    task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,7 +8,7 @@ import requests
 
 if __name__ == "__main__":
     r = requests.get(sys.argv[1])
-    if r.status_code == 200:
+    if r.status_code < 400:
         print(r.text)
     else:
         print(f'Error code: {e.status_code}')
