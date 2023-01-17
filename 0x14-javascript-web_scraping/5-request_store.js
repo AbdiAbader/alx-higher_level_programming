af2bfea69fdc8a2c533c512e2654f7bb280d83ac
AbdiAbader <abdiprocode@gmail.com>
2023-01-17 15:07:51 +0300

    fix semi standard

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -4,17 +4,17 @@ const request = require('request');
 const fs = require('fs');
 
 if (process.argv.length < 4) {
-    console.log('Usage: ./5-request_store.js URL FILE');
-    process.exit(1);
-    }
+  console.log('Usage: ./5-request_store.js URL FILE');
+  process.exit(1);
+}
 request(process.argv[2], (err, response, body) => {
-    if (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(process.argv[3], body, 'utf8', (err) => {
+      if (err) {
         console.log(err);
-    } else {
-        fs.writeFile(process.argv[3], body, 'utf8', (err) => {
-            if (err) {
-                console.log(err);
-            }
-        });
-    }
+      }
+    });
+  }
 });
