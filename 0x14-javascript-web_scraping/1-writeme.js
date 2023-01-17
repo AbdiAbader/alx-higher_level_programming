cefe7832e09c8673f2541c9e091f0e70da89379e
AbdiAbader <abdiprocode@gmail.com>
2023-01-17 13:41:24 +0300

    task 2

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+if (process.argv.length < 4) {
+    console.log('Usage: ./1-writeme.js FILE DATA');
+    process.exit(1);
+    }
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', (err) => {
+    if (err) {
+        console.log(err);
+    }
+    });
---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const fs = require('fs');
+const request = require('request');
+
+if (process.argv.length < 3) {
+    console.log('Usage: ./2-statuscode.js URL');
+    process.exit(1);
+    }
+request(process.argv[2], (err, response) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('code: ' + response.statusCode);
+    }
+    }
+);
